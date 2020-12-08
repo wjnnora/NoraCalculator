4bd2bf43e4243598a5225717ef58908f077e8119
Wellington Nora <wellington_nora94@hotmail.com>
2020-12-08 20:25:58 -0300

    feat(*): adiciona o arquivo de configuração

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="NoraCalculator",
+    version="0.0.1",
+    author="Wellington Nora",
+    author_email="wellington_nora94@hotmail.com",
+    url="https://github.com/wjnnora/SamplePyPi",
+    description="An example package PyPi using math functions.",
+    packages=["NoraCalculator"],
+    zip_safe=False
+)
